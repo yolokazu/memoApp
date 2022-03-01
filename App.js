d9a734e46f812bde68b7be24f52e27246eed80fa
Kazunari Watanabe <kazunariwatanabe@Kazunaris-MacBook-Air.local>
2022-03-01 23:01:27 +0900

    implemented firebase authentication

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import firebase from 'firebase';
 
 import LogInScreen from './src/screens/LogInScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
@@ -9,8 +10,13 @@ import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
+import { firebaseConfig } from './env';
 
 const Stack = createStackNavigator();
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
+import firebase from 'firebase';
+
 import Button from '../components/Button';
 
 const SignUpScreen = (props) => {
   const { navigation } = props;
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+
+  const handleOnPress = () => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        console.log(user.uid);
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'List' }],
+        });
+      })
+      .catch((error) => {
+        console.log(error.code, error.message);
+        Alert.alert(error.message);
+      });
+  };
 
   return (
     <View style={styles.container}>
@@ -31,12 +49,7 @@ const SignUpScreen = (props) => {
         />
         <Button
           label='Submit'
-          onPress={() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'List' }],
-            });
-          }}
+          onPress={handleOnPress}
         />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already resistered ?</Text>
